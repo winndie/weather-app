78bd12efd6a6d1a95721870dce5badd08a296959
Winndie Fan <winndie@gmail.com>
2023-08-20 16:11:07 +0100

    Framework7 param

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect } from 'react'
-import './App.css'
 import Framework7 from 'framework7/lite'
 import Framework7React, { List, ListItem, Navbar, Page, Searchbar, Subnavbar } from 'framework7-react'
 import { RootState, useAppDispatch } from './store'
 import { useSelector } from 'react-redux'
 import { getWeatherBySearchText } from './api'
 
-const App:React.FC=()=> {
+const Home:React.FC=()=> {
 
   Framework7.use(Framework7React)
   const dispatch = useAppDispatch()
@@ -39,4 +38,4 @@ const App:React.FC=()=> {
     )
 }
 
-export default App;
+export default Home;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Home from './Home';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store';
+import { Framework7Parameters } from 'framework7/types';
+import {App} from 'framework7-react';
+
+const f7params = {
+  name: 'Weather App',
+  theme: 'md',
+  statusbar: {
+    iosOverlaysWebView: true,
+    androidOverlaysWebView: false,
+  },
+
+} as Framework7Parameters;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,7 +24,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+    <Home/>
     </Provider>
   </React.StrictMode>
 );
